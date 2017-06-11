c6e5466c7d6d2479d2dbd9db82ebd41bc957abfe
garib Ga <garild@o2.pl>
2017-06-11 13:39:30 +0200

    Small changes

---
+++ SkyReg/SkyReg/Forms/SplashScreen/SplashScreen.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SkyReg.Forms.SplashScreen
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.progressBar = new MetroFramework.Controls.MetroProgressBar();
+            this.cbDbExists = new MetroFramework.Controls.MetroCheckBox();
+            this.cbLoadSettings = new MetroFramework.Controls.MetroCheckBox();
+            this.metroCheckBox2 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroCheckBox3 = new MetroFramework.Controls.MetroCheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = null;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.White;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // progressBar
+            // 
+            this.progressBar.HideProgressText = false;
+            this.progressBar.Location = new System.Drawing.Point(33, 450);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.ProgressBarStyle = System.Windows.Forms.ProgressBarStyle.Blocks;
+            this.progressBar.Size = new System.Drawing.Size(655, 23);
+            this.progressBar.Step = 25;
+            this.progressBar.Style = MetroFramework.MetroColorStyle.Black;
+            this.progressBar.TabIndex = 0;
+            // 
+            // cbDbExists
+            // 
+            this.cbDbExists.AutoSize = true;
+            this.cbDbExists.Location = new System.Drawing.Point(33, 345);
+            this.cbDbExists.Name = "cbDbExists";
+            this.cbDbExists.Size = new System.Drawing.Size(266, 15);
+            this.cbDbExists.TabIndex = 1;
+            this.cbDbExists.Text = "Baza danych SkyReg zainstalowana poprawnie";
+            this.cbDbExists.UseVisualStyleBackColor = true;
+            // 
+            // cbLoadSettings
+            // 
+            this.cbLoadSettings.AutoSize = true;
+            this.cbLoadSettings.Location = new System.Drawing.Point(33, 366);
+            this.cbLoadSettings.Name = "cbLoadSettings";
+            this.cbLoadSettings.Size = new System.Drawing.Size(215, 15);
+            this.cbLoadSettings.TabIndex = 1;
+            this.cbLoadSettings.Text = "Ładowanie konfiguracji bazy danych";
+            this.cbLoadSettings.UseVisualStyleBackColor = true;
+            // 
+            // metroCheckBox2
+            // 
+            this.metroCheckBox2.AutoSize = true;
+            this.metroCheckBox2.Location = new System.Drawing.Point(33, 387);
+            this.metroCheckBox2.Name = "metroCheckBox2";
+            this.metroCheckBox2.Size = new System.Drawing.Size(266, 15);
+            this.metroCheckBox2.TabIndex = 1;
+            this.metroCheckBox2.Text = "Baza danych SkyReg zainstalowana poprawnie";
+            this.metroCheckBox2.UseVisualStyleBackColor = true;
+            // 
+            // metroCheckBox3
+            // 
+            this.metroCheckBox3.AutoSize = true;
+            this.metroCheckBox3.Location = new System.Drawing.Point(33, 408);
+            this.metroCheckBox3.Name = "metroCheckBox3";
+            this.metroCheckBox3.Size = new System.Drawing.Size(266, 15);
+            this.metroCheckBox3.TabIndex = 1;
+            this.metroCheckBox3.Text = "Baza danych SkyReg zainstalowana poprawnie";
+            this.metroCheckBox3.UseVisualStyleBackColor = true;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 515);
+            this.Controls.Add(this.metroCheckBox3);
+            this.Controls.Add(this.metroCheckBox2);
+            this.Controls.Add(this.cbLoadSettings);
+            this.Controls.Add(this.cbDbExists);
+            this.Controls.Add(this.progressBar);
+            this.Name = "SplashScreen";
+            this.Text = "SplashScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroProgressBar progressBar;
+        private MetroFramework.Controls.MetroCheckBox cbDbExists;
+        private MetroFramework.Controls.MetroCheckBox cbLoadSettings;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox2;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox3;
+    }
+}
---
+++ SkyReg/SkyReg/Forms/SplashScreen/SplashScreen.cs
@@ -0,0 +1,116 @@
+﻿using AC.ExtendedRenderer.Toolkit;
+using DataLayer;
+using DataLayer.Utils;
+using MetroFramework.Forms;
+using SkyReg.Utils;
+using System;
+using Msg = AC.ExtendedRenderer.Toolkit.KryptonMessageBox;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SkyReg.Forms.SplashScreen
+{
+    public partial class SplashScreen : MetroForm
+    {
+        private Timer _timer = new Timer();
+        private bool IsDbExists = false;
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+            SkyRegUser.GlobalPathFile = Environment.GetFolderPath((Environment.SpecialFolder.LocalApplicationData)) + @"\SkyReg";
+            SkyRegUser.DatabaseConfigFile = string.Format("{0}\\DatabaseConfig.xml", SkyRegUser.GlobalPathFile);
+            SkyRegUser.UserConfigFile = string.Format("{0}\\UserConfig.xml", SkyRegUser.GlobalPathFile);
+            SkyRegUser.LocalMachineName = Environment.MachineName;
+
+            StarLoading();
+        }
+
+        private void StarLoading()
+        {
+            this.Opacity = 100;
+            _timer.Enabled = true;
+            _timer.Interval = 4000;
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+        private void ValidateTask()
+        {
+            
+        }
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void LoadSettings()
+        {
+            try
+            {
+                Version curVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                string version = curVersion.ToString();
+
+                SkyRegUser.AppVer = version;
+
+                CommonMethods.CheckInternetConnection();
+
+                CreateSkyregFolder();
+                CheckDatabaseConfig();
+
+                using (TextReader tr = new StreamReader(SkyRegUser.DatabaseConfigFile))
+                {
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(SkyRegUser.DatabaseConfigFile);
+                    XmlSerializer deserializer;
+
+                    //odczyt ustawień z nowej wersji pliku
+                    var ConfigSettings = new DatabaseAccess();
+                    deserializer = new XmlSerializer(ConfigSettings.GetType());
+                    ConfigSettings = ((DatabaseAccess)deserializer.Deserialize(tr));
+                    tr.Close();
+                    ConfigSettings.Password = ConfigSettings.Password.DecryptString();
+                    ConfigSettings.User = ConfigSettings.User.DecryptString();
+                    new DatabaseConfig(ConfigSettings);
+                }
+
+
+                if (!string.IsNullOrEmpty(DatabaseConfig.ConnectionString))
+                {
+                    FirstTimeRun.CheckAndAdd();
+                }
+            }
+
+            catch (Exception)
+            {
+                this.DialogResult = DialogResult.Cancel;
+            }
+
+        }
+
+        private void CheckDatabaseConfig()
+        {
+            if (!File.Exists(SkyRegUser.DatabaseConfigFile))
+            {
+                IsDbExists = false;
+            }
+            else
+                IsDbExists = true;
+
+        }
+        private void CreateSkyregFolder()
+        {
+            if (!Directory.Exists(SkyRegUser.GlobalPathFile))
+            {
+                Directory.CreateDirectory(SkyRegUser.GlobalPathFile);
+            }
+
+        }
+    }
+}
